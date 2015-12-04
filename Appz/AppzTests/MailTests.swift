b16aa57a5e50a1c629f10a4eb9a42a81146b3fc0
Suraj Shirvankar <surajshirvankar@gmail.com>
2015-12-05 01:22:57 +0400

    Added mail and messages
    Fix project files
    
    Escape string in the mail scheme
    
    Move to swift structs
    
    Fix minor issues
    Move Email to global namespace
    
    Escape input

---
+++ Appz/AppzTests/MailTests.swift
@@ -0,0 +1,52 @@
+//
+//  MailTests.swift
+//  Appz
+//
+//  Created by Suraj Shirvankar on 12/5/15.
+//  Copyright © 2015 kitz. All rights reserved.
+//
+
+import XCTest
+@testable import Appz
+
+class MailTests: XCTestCase {
+    
+    let appCaller = ApplicationCallerMock()
+    
+    func testConfiguration() {
+        
+        let mail = AvailableApplications.Mail()
+        XCTAssertEqual(mail.scheme, "mailto:")
+        XCTAssertEqual(mail.fallbackURL, "")
+    }
+    
+    func testEmail() {
+        
+        let email = "test@example.com"
+        let subject = "subject"
+        let body = "body"
+        let action = AvailableApplications.Mail.Action.Compose(email: Email(to: email, subject: subject, body: body))
+        
+        
+        XCTAssertEqual(action.path, "\(email)?subject=\(subject)&body=\(body)")
+        
+    }
+    
+    func testEmptyEmail() {
+        
+        let email = "test@example.com"
+        let action = AvailableApplications.Mail.Action.Compose(email: Email(to: email))
+        
+        
+        XCTAssertEqual(action.path, "\(email)?subject=&body=")
+        
+    }
+    
+    func testSubject() {
+        
+        let subject = "test"
+        let action = AvailableApplications.Mail.Action.Compose(email: Email(subject: subject))
+        
+        XCTAssertEqual(action.path, "?subject=\(subject)&body=")
+    }
+}
