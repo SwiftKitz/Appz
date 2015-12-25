26aeb2f855eeb2ce3fd1bddc3039b7522e81ba97
Mariam <mrm259@gmail.com>
2015-12-25 20:46:21 +0300

    Facebook support is added successfully with a test :)

---
+++ Appz/FacebookTests/FacebookTests.swift
@@ -0,0 +1,58 @@
+//
+//  FacebookTests.swift
+//  FacebookTests
+//
+//  Created by Mariam AlJamea on 12/25/15.
+//  Copyright © 2015 kitz. All rights reserved.
+//
+
+import XCTest
+@testable import Appz
+
+class FacebookTests: XCTestCase {
+    
+    let appCaller = ApplicationCallerMock()
+    
+    func testConfiguration() {
+        
+        let twitter = Applications.Facebook()
+        XCTAssertEqual(twitter.scheme, "fb:")
+        XCTAssertEqual(twitter.fallbackURL, "https://www.facebook.com/")
+    }
+    
+    func testOpen() {
+        
+        let action = Applications.Facebook.Action.Open
+        
+        XCTAssertEqual(action.paths.app.pathComponents, ["app"])
+        XCTAssertEqual(action.paths.app.queryParameters, [:])
+        XCTAssertEqual(action.paths.web, Path())
+    }
+    
+    func testProfile() {
+        
+        let action = Applications.Facebook.Action.Profile
+        
+        XCTAssertEqual(action.paths.app.pathComponents, ["profile"])
+        XCTAssertEqual(action.paths.app.queryParameters, [:])
+        XCTAssertEqual(action.paths.web, Path())
+    }
+    
+    func testNotifications() {
+        
+        let action = Applications.Facebook.Action.Notifications
+        
+        XCTAssertEqual(action.paths.app.pathComponents, ["notifications"])
+        XCTAssertEqual(action.paths.app.queryParameters, [:])
+        XCTAssertEqual(action.paths.web, Path())
+    }
+    
+    func testFeed() {
+        
+        let action = Applications.Facebook.Action.Feed
+        
+        XCTAssertEqual(action.paths.app.pathComponents, ["feed"])
+        XCTAssertEqual(action.paths.app.queryParameters, [:])
+        XCTAssertEqual(action.paths.web, Path())
+    }
+}
