7a9c6e80af8e11a46b57111550f825c259c0dedd
Mariam <mrm259@gmail.com>
2015-12-26 23:08:18 +0300

    Added Google Maps and Pinterest with testing them and adding them to README file

---
+++ Appz/AppzTests/AppsTests/GoogleMapsTests.swift
@@ -0,0 +1,71 @@
+//
+//  GoogleMapsTests.swift
+//  GoogleMapsTests
+//
+//  Created by Mariam AlJamea on 12/24/15.
+//  Copyright © 2015 kitz. All rights reserved.
+//
+
+import XCTest
+@testable import Appz
+
+class GoogleMapsTests: XCTestCase {
+    
+    let appCaller = ApplicationCallerMock()
+    
+    func testConfiguration() {
+        
+        let youtube = Applications.GoogleMaps()
+        XCTAssertEqual(youtube.scheme, "comgooglemaps:")
+        XCTAssertEqual(youtube.fallbackURL, "https://maps.google.com/")
+    }
+    
+    func testOpen() {
+        
+        let action = Applications.GoogleMaps.Action.Open
+        
+        XCTAssertEqual(action.paths.app.pathComponents, ["app"])
+        XCTAssertEqual(action.paths.app.queryParameters, [:])
+        XCTAssertEqual(action.paths.web, Path())
+    }
+    
+    func testDisplayDirections() {
+        
+        let saddr = "Surra"
+        let daddr = "infocenter"
+        let directionsmode = "driving"
+        let action = Applications.GoogleMaps.Action.DisplayDirections(
+                                                                    saddr: saddr,
+                                                                    daddr: daddr,
+                                                                    directionsmode:
+                                                                    directionsmode)
+        
+        XCTAssertEqual(action.paths.app.pathComponents, [""])
+        XCTAssertEqual(action.paths.app.queryParameters,
+            [
+            "saddr": saddr,
+            "daddr": daddr,
+            "directionsmode": directionsmode,
+            ])
+        
+        XCTAssertEqual(action.paths.web.pathComponents, [""])
+        XCTAssertEqual(action.paths.web.queryParameters,
+            [
+            "saddr": saddr,
+            "daddr": daddr,
+            "directionsmode": directionsmode,
+            ])
+    }
+    
+    func testSearch() {
+        
+        let q = "Pizza"
+        let action = Applications.GoogleMaps.Action.Search(q: q)
+        
+        XCTAssertEqual(action.paths.app.pathComponents, [""])
+        XCTAssertEqual(action.paths.app.queryParameters, ["q": q,])
+        
+        XCTAssertEqual(action.paths.web.pathComponents, [""])
+        XCTAssertEqual(action.paths.web.queryParameters, ["q": q])
+    }
+}
