aa8c00d4040b20e10788f219e4a9bed8c13e088b
Mariam <mrm259@gmail.com>
2016-01-09 20:12:15 +0300

    Added Tumblr, Uber, and DayCost.

---
+++ Appz/Appz/Apps/DayCost.swift
@@ -0,0 +1,46 @@
+//
+//  DayCost.swift
+//  Pods
+//
+//  Created by Mariam AlJamea on 1/9/16.
+//  Copyright © 2015 kitz. All rights reserved.
+//
+
+public extension Applications {
+    
+    public struct DayCost: ExternalApplication {
+        
+        public typealias ActionType = Applications.DayCost.Action
+        
+        public let scheme = "DayCost:"
+        public let fallbackURL = "https://www.facebook.com/iDaycost/"
+        
+        public init() {}
+    }
+}
+
+// MARK: - Actions
+
+public extension Applications.DayCost {
+    
+    public enum Action {
+        case Open
+    }
+}
+
+extension Applications.DayCost.Action: ExternalApplicationAction {
+    
+    public var paths: ActionPaths {
+        
+        switch self {
+        case .Open:
+            return ActionPaths(
+                app: Path(
+                    pathComponents: ["app"],
+                    queryParameters: [:]
+                ),
+                web: Path()
+            )
+        }
+    }
+}
---
+++ Appz/Appz/Apps/Tumblr.swift
@@ -0,0 +1,99 @@
+//
+//  Tumblr.swift
+//  Pods
+//
+//  Created by Mariam AlJamea on 1/9/16.
+//  Copyright © 2015 kitz. All rights reserved.
+//
+
+public extension Applications {
+    
+    public struct Tumblr: ExternalApplication {
+        
+        public typealias ActionType = Applications.Tumblr.Action
+        
+        public let scheme = "tumblr:"
+        public let fallbackURL = "https://www.tumblr.com"
+        
+        public init() {}
+    }
+}
+
+// MARK: - Actions
+
+public extension Applications.Tumblr {
+    
+    public enum Action {
+        case Open
+        case Dashboard
+        case Explore
+        case Activity
+        case Blog
+        case Tag(tag: String)
+    }
+}
+
+extension Applications.Tumblr.Action: ExternalApplicationAction {
+    
+    public var paths: ActionPaths {
+        
+        switch self {
+        case .Open:
+            return ActionPaths(
+                app: Path(
+                    pathComponents: ["app"],
+                    queryParameters: [:]
+                ),
+                web: Path()
+            )
+            
+        case .Dashboard:
+            return ActionPaths(
+                app: Path(
+                    pathComponents: ["x-callback-url", "dashboard"],
+                    queryParameters: [:]
+                ),
+                web: Path()
+            )
+            
+        case .Explore:
+            return ActionPaths(
+                app: Path(
+                    pathComponents: ["x-callback-url", "explore"],
+                    queryParameters: [:]
+                ),
+                web: Path()
+            )
+            
+        case .Activity:
+            return ActionPaths(
+                app: Path(
+                    pathComponents: ["x-callback-url", "activity"],
+                    queryParameters: [:]
+                ),
+                web: Path()
+            )
+            
+        case .Blog:
+            return ActionPaths(
+                app: Path(
+                    pathComponents: ["x-callback-url", "blog"],
+                    queryParameters: [:]
+                ),
+                web: Path()
+            )
+          
+        case .Tag(let tag):
+            return ActionPaths(
+                app: Path(
+                    pathComponents: ["x-callback-url", "tag"],
+                    queryParameters:
+                    [
+                        "tag":tag
+                    ]
+                ),
+                web: Path()
+            )
+        }
+    }
+}
