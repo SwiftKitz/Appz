0173f5e6a256fbc455d041542278b5282885d5da
Sai Kambampati <ssv1065@gmail.com>
2018-10-02 02:48:35 -0700

    Added The Views News (#136)

---
+++ Appz/Appz/Apps/Views.swift
@@ -0,0 +1,45 @@
+//
+//  Views.swift
+//  Pods
+//
+//  Created by Sai Kambampati on 10/1/18.
+//  Copyright © 2018 Sai Kambampati. All rights reserved.
+//
+public extension Applications {
+    
+    public struct Views: ExternalApplication {
+        
+        public typealias ActionType = Applications.Views.Action
+        
+        public let scheme = "views:"
+        public let fallbackURL = "http://www.saikambampati.com/views"
+        public let appStoreId = "1322754821"
+        
+        public init() {}
+    }
+}
+
+// MARK: - Actions
+public extension Applications.Views {
+    
+    public enum Action {
+        case open
+    }
+}
+
+extension Applications.Views.Action: ExternalApplicationAction {
+    
+    public var paths: ActionPaths {
+        
+        switch self {
+        case .open:
+            return ActionPaths(
+                app: Path(
+                    pathComponents: ["app"],
+                    queryParameters: [:]
+                ),
+                web: Path()
+            )
+        }
+    }
+}
